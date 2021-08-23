29408e335fd309f04bddfc6e48d23f0d270342a6
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-08-23 11:48:12 -0700

    Updates

---
+++ precache-manifest.68251400f11cf20f7d8e91a730fea514.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8ffb1389c58eb62057",
+    "url": "/HireMe/static/css/main.c7a01067.chunk.css"
+  },
+  {
+    "revision": "7b8ffb1389c58eb62057",
+    "url": "/HireMe/static/js/main.a86d0c4a.chunk.js"
+  },
+  {
+    "revision": "02da54ac8e52ad1968be",
+    "url": "/HireMe/static/js/runtime~main.86b381ee.js"
+  },
+  {
+    "revision": "3dd9bb10ad241bc2831e",
+    "url": "/HireMe/static/css/2.6ca26d17.chunk.css"
+  },
+  {
+    "revision": "3dd9bb10ad241bc2831e",
+    "url": "/HireMe/static/js/2.d278efa8.chunk.js"
+  },
+  {
+    "revision": "e1410516a61829ecd3eeb7387f0fe711",
+    "url": "/HireMe/static/media/tools.e1410516.jpg"
+  },
+  {
+    "revision": "a49b22b8483255e09f418806b126ee7a",
+    "url": "/HireMe/static/media/png-tree-bulb.a49b22b8.png"
+  },
+  {
+    "revision": "4fc38738f924f5b9d6757599a6215728",
+    "url": "/HireMe/static/media/wood-round.4fc38738.jpg"
+  },
+  {
+    "revision": "f4c1651b2449cf70d490877c7ca87e00",
+    "url": "/HireMe/static/media/creativeDesignScreen.f4c1651b.png"
+  },
+  {
+    "revision": "7b8dc0e037b6c5ad377b1a667810a6d8",
+    "url": "/HireMe/static/media/cabinet-transparent.7b8dc0e0.png"
+  },
+  {
+    "revision": "36169e0e024534b20d8b8f5a2c1bc35a",
+    "url": "/HireMe/static/media/brown.36169e0e.png"
+  },
+  {
+    "revision": "db52b685ae1e8a6aa30e489379968978",
+    "url": "/HireMe/index.html"
+  }
+];
